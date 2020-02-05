26da66e96626de3e3f2488858505d40bb2306e8b
Dknwhe <cai.mac@hotmail.com>
2020-02-05 15:18:18 +0100

    31 test done

--- CodingAssignment/src/main/java/se/ecutb/service/PersonDTOImpl.java
+++ CodingAssignment/src/main/java/se/ecutb/service/PersonDTOImpl.java
@@ -1,5 +1,6 @@
 package se.ecutb.service;
 
+import org.springframework.stereotype.Component;
 import se.ecutb.dto.PersonDto;
 import se.ecutb.dto.PersonDtoWithTodo;
 import se.ecutb.model.Person;
@@ -7,12 +8,14 @@ import se.ecutb.model.Todo;
 
 import java.util.List;
 
+
+@Component
 public class PersonDTOImpl implements PersonDtoConversionService {
 
 
     @Override
     public PersonDto convertToPersonDto(Person person) {
-        return null;
+        return new PersonDto(person.getPersonId(),person.getFirstName(),person.getLastName());
     }
 
     @Override
